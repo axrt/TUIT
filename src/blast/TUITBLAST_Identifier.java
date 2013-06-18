54905e5b7054c172ce08b865217f71e1f2c8c3ab
axrt <alexander.tuzhikov@gmail.com>
2013-06-18 15:36:31 +0400

    Generification corrected

--- src/blast/TUITBLAST_Identifier.java
+++ src/blast/TUITBLAST_Identifier.java
@@ -13,11 +13,11 @@ import java.util.Map;
  * //TODO:Document as soon as works
  * To change this template use File | Settings | File Templates.
  */
-public class TUITBLAST_Identifier<T extends NucleotideFasta> extends BLAST_Identifier {
+public class TUITBLAST_Identifier extends BLAST_Identifier {
 
     protected int batchSize;
 
-    protected TUITBLAST_Identifier(List query, List<String> query_IDs, File tempDir, File executive, String[] parameterList, TUITFileOperator identifierFileOperator, Connection connection, Map<Ranks, TUITCutoffSet> cutoffSetMap, int batchSize) {
+    protected TUITBLAST_Identifier(List<NucleotideFasta> query, List<String> query_IDs, File tempDir, File executive, String[] parameterList, TUITFileOperator identifierFileOperator, Connection connection, Map<Ranks, TUITCutoffSet> cutoffSetMap, int batchSize) {
         super(query, query_IDs, tempDir, executive, parameterList, identifierFileOperator, connection, cutoffSetMap);
         this.batchSize = batchSize;
     }
@@ -27,10 +27,10 @@ public class TUITBLAST_Identifier<T extends NucleotideFasta> extends BLAST_Ident
 
     }
 
-    public static <T extends NucleotideFasta> TUITBLAST_Identifier newInstanceFromFileOperator(
+    public static TUITBLAST_Identifier newInstanceFromFileOperator(
             File tempDir, File executive, String[] parameterList, TUITFileOperator identifierFileOperator,
             Connection connection, Map<Ranks, TUITCutoffSet> cutoffSetMap, int batchSize) throws Exception {
-        List<T> batch = identifierFileOperator.nextBatch(batchSize);
+        List<NucleotideFasta> batch = identifierFileOperator.nextBatch(batchSize);
         if (batch != null) {
             TUITBLAST_Identifier tuitblastIdentifier = new TUITBLAST_Identifier(batch, null, tempDir, executive, parameterList, identifierFileOperator, connection, cutoffSetMap, batchSize);
             return tuitblastIdentifier;
